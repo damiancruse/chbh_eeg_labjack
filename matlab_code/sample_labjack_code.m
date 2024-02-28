33395a0395ce9ac42acc989a8132b378681dcc10
froemero <romy.froemer@gmail.com>
2024-02-28 15:02:58 +0000

    Update sample_labjack_code.m
    update re pin reset

--- matlab_code/sample_labjack_code.m
+++ matlab_code/sample_labjack_code.m
@@ -8,9 +8,11 @@ params.ext.L = L;
 
 %% send a marker
 trig_code = 1; % the code you want for your marker in your EEG recording
-lab_put_code_sa(L,trig_code); % send the trigger
+lab_put_code_sa(params.ext.L,trig_code); % send the trigger - within lab_put_code_sa, the pins are reset to zero after a wait of 0.002 s. 
+
+%% at slow sampling rates, triggers can be lost; to circumvent this issue, manual resetting has been used in the past. 
 WaitSecs(.05); % requires Psychtoolbox in the path
-lab_put_code_sa(L,0); % reset to zero so that the marker is discrete
+lab_put_code_sa(params.ext.L,0); % reset to zero so that the marker is discrete
 
 %% close the LabJack
 params = lab_close_sa(params);
